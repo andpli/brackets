3414ff3d2ddaf3c663f260030b4030d43cb335a9
andpli <pandre@tut.by>
2023-08-15 16:57:12 +0300

    init: brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let strNew = '';
+  while (strNew != str && str != '') {
+     strNew = str;
+     bracketsConfig.forEach(element => {
+     str = str.replace(element[0] + element[1],"");
+    });
+  }
+
+  return str == "";
 }
